00be43417f61c5ce87f17c201709fbe3d1c4516e
VirtualMaestro <virtualmaestro@gmail.com>
2019-09-08 14:46:55 +0200

    add basic scene controller

---
+++ StubbUnity/StubbUnity/Src/SceneController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StubbUnity
+{
+    using UnityEngine;
+    public class SceneController : MonoBehaviour
+    {
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
