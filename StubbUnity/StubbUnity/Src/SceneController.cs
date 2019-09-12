791c5d1317da5ea06ff82912448bd36e0f53ddc1
VirtualMaestro <virtualmaestro@gmail.com>
2019-09-12 12:51:19 +0200

    Add simple scene controller

--- StubbUnity/StubbUnity/Src/SceneController.cs
+++ StubbUnity/StubbUnity/Src/SceneController.cs
@@ -5,9 +5,39 @@ namespace StubbUnity
     using UnityEngine;
     public class SceneController : MonoBehaviour
     {
+        private uint MAX_OBJECTS_ON_SCENE = 2;
+    
+        private GameObject _sceneContentRoot;
         private void Start()
         {
-            throw new NotImplementedException();
+            
         }
+        
+        public void ShowContent()
+        {
+            if (!IsContentShown)
+            {
+                _sceneContentRoot.SetActive(true);
+            }
+        }
+
+        public void HideContent()
+        {
+            if (IsContentShown)
+            {
+                _sceneContentRoot.SetActive(false);
+            }
+        }
+
+        public bool IsContentShown
+        {
+            get { return _sceneContentRoot.activeSelf; }
+        }
+
+        private string SceneName
+        {
+            get { return gameObject.scene.name; }
+        }
+
     }
 }
