0fb90c4af4a9991682cd2e4000cd4d47e08a2a41
VirtualMaestro <virtualmaestro@gmail.com>
2019-10-10 12:30:52 +0200

    Implement SceneService

---
+++ StubbUnity/StubbUnity/Src/Scenes/SceneLoadingProgress.cs
@@ -0,0 +1,22 @@
+using StubbFramework.Scenes;
+using UnityEngine;
+
+namespace StubbUnity.Scenes
+{
+    public class SceneLoadingProgress : ISceneLoadingProgress
+    {
+        public ISceneName SceneName { get; }
+        public bool IsComplete => _async.isDone;
+        public float Progress => _async.progress;
+        public object Payload { get; }
+
+        private readonly AsyncOperation _async;
+
+        public SceneLoadingProgress(in ISceneName name, AsyncOperation payload)
+        {
+            SceneName = name;
+            Payload = payload;
+            _async = payload;
+        }
+    }
+}
--- StubbUnity/StubbUnity/Src/Services/SceneService.cs
+++ StubbUnity/StubbUnity/Src/Services/SceneService.cs
@@ -1,19 +1,58 @@
+using System.Collections.Generic;
+using StubbFramework.Scenes;
 using StubbFramework.Scenes.Configurations;
 using StubbFramework.Services;
+using StubbUnity.Scenes;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace StubbUnity.Services
 {
     public class SceneService : ISceneService
     {
-        public void Load(ILoadingSceneConfig config)
+        public ISceneLoadingProgress[] Load(ILoadingScenesConfig config)
         {
-            SceneManager.LoadSceneAsync(config.SceneName, config.IsAdditive ? LoadSceneMode.Additive: LoadSceneMode.Single);
+            SceneLoadingProgress[] progresses = new SceneLoadingProgress[config.NumScenes];
+            int index = 0;
+            bool allowSceneActivation = !config.IsActivatingAll;
+
+            foreach (var sceneConfig in config)
+            {
+                var async = SceneManager.LoadSceneAsync(sceneConfig.Name.FullName, sceneConfig.IsAdditive ? LoadSceneMode.Additive: LoadSceneMode.Single);
+                async.allowSceneActivation = allowSceneActivation;
+                progresses[index++] = new SceneLoadingProgress(sceneConfig.Name, async);
+            }
+
+            return progresses;
         }
 
-        public void Unload(string sceneName)
+        public void Unload(ISceneName sceneName)
         {
-            
+            SceneManager.UnloadSceneAsync(sceneName.FullName, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+        }
+
+        public void Unload(IList<ISceneName> sceneNames)
+        {
+            foreach (var name in sceneNames)
+            {
+                Unload(name);
+            }
+           
+            Resources.UnloadUnusedAssets();
+        }
+
+        public void Activate(ISceneLoadingProgress[] progresses)
+        {
+            foreach (var progress in progresses)
+            {
+                Activate(progress);
+            }
+        }
+
+        public void Activate(ISceneLoadingProgress progress)
+        {
+             AsyncOperation async = (AsyncOperation) progress.Payload;
+             async.allowSceneActivation = true;
         }
     }
 }
