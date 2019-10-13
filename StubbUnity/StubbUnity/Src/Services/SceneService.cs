6e2926f185af7743ffec29053cce67089f6c2f12
VirtualMaestro <virtualmaestro@gmail.com>
2019-10-13 20:16:10 +0200

    New methods and testing logging

--- StubbUnity/StubbUnity/Src/Services/SceneService.cs
+++ StubbUnity/StubbUnity/Src/Services/SceneService.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using StubbFramework.Scenes;
 using StubbFramework.Scenes.Configurations;
 using StubbFramework.Services;
+using StubbUnity.Logging;
 using StubbUnity.Scenes;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,6 +11,16 @@ namespace StubbUnity.Services
 {
     public class SceneService : ISceneService
     {
+        public SceneService()
+        {
+            SceneManager.sceneLoaded += _SceneLoaded;
+        }
+
+        private void _SceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            Debug.LogWarning($"SceneService._SceneLoaded. Scene name: {scene.name}, mode: {mode.ToString()}");
+        }
+        
         public ISceneLoadingProgress[] Load(in ILoadingScenesConfig config)
         {
             SceneLoadingProgress[] progresses = new SceneLoadingProgress[config.NumScenes];
@@ -18,9 +29,8 @@ namespace StubbUnity.Services
 
             foreach (var sceneConfig in config)
             {
-                var async = SceneManager.LoadSceneAsync(sceneConfig.Name.FullName, sceneConfig.IsAdditive ? LoadSceneMode.Additive: LoadSceneMode.Single);
-                async.allowSceneActivation = allowSceneActivation;
-                progresses[index++] = new SceneLoadingProgress(sceneConfig.Name, async);
+                var async = SceneManager.LoadSceneAsync(sceneConfig.Name.FullName, sceneConfig.IsSingle ? LoadSceneMode.Single: LoadSceneMode.Additive);
+                progresses[index++] = new SceneLoadingProgress(sceneConfig, async);
             }
 
             return progresses;
@@ -41,18 +51,32 @@ namespace StubbUnity.Services
             Resources.UnloadUnusedAssets();
         }
 
-        public void Activate(in ISceneLoadingProgress[] progresses)
+        public void LoadingComplete(in ISceneLoadingProgress[] progresses)
         {
+            log.Warn("SceneService.LoadingComplete: progresses num " + progresses.Length);
             foreach (var progress in progresses)
             {
-                Activate(progress);
+                LoadingComplete(progress);
             }
         }
 
-        public void Activate(in ISceneLoadingProgress progress)
+        public void LoadingComplete(in ISceneLoadingProgress progress)
         {
-             AsyncOperation async = (AsyncOperation) progress.Payload;
-             async.allowSceneActivation = true;
+            log.Warn($"Scene name: {progress.Config.Name}, Progress: {progress.Progress}, IsComplete: {progress.IsComplete}");
         }
+
+//        public void Activate(in ISceneLoadingProgress[] progresses)
+//        {
+//            foreach (var progress in progresses)
+//            {
+//                Activate(progress);
+//            }
+//        }
+//
+//        public void Activate(in ISceneLoadingProgress progress)
+//        {
+//             AsyncOperation async = (AsyncOperation) progress.Payload;
+//             async.allowSceneActivation = true;
+//        }
     }
 }
