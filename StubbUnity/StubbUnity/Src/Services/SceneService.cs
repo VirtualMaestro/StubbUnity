eb68b4d9e513a04b13490279c013fbd27fb2e335
VirtualMaestro <virtualmaestro@gmail.com>
2019-10-17 12:42:22 +0200

    Update to the latest API

--- StubbUnity/StubbUnity/Src/Services/SceneService.cs
+++ StubbUnity/StubbUnity/Src/Services/SceneService.cs
@@ -1,6 +1,5 @@
 using Leopotam.Ecs;
 using StubbFramework;
-using StubbFramework.Common.Components;
 using StubbFramework.Scenes;
 using StubbFramework.Scenes.Components;
 using StubbFramework.Scenes.Configurations;
@@ -14,19 +13,19 @@ namespace StubbUnity.Services
 {
     public class SceneService : ISceneService
     {
-        private EcsFilter<SceneComponent, NewEntityComponent> _newScenesFilter;
+        private EcsFilter<SceneComponent, NewSceneMarkerComponent> _newScenesFilter;
         
         public SceneService()
         {
-            _newScenesFilter = (EcsFilter<SceneComponent, NewEntityComponent>) Stubb.World.GetFilter(typeof(EcsFilter<SceneComponent, NewEntityComponent>));
+            _newScenesFilter = (EcsFilter<SceneComponent, NewSceneMarkerComponent>) Stubb.World.GetFilter(typeof(EcsFilter<SceneComponent, NewSceneMarkerComponent>));
             SceneManager.sceneLoaded += _SceneLoaded;
         }
 
         private void _SceneLoaded(Scene scene, LoadSceneMode mode)
         {
             _SceneVerification(scene);
-            // add new SceneComponent with current loaded scene to the ECS layer
-            Stubb.World.NewEntityWith<SceneComponent, NewEntityComponent>(out var sceneComponent, out var newEntityComponent);
+            // add new SceneComponent with current loaded scene to the ECS layer and this scene as new
+            Stubb.World.NewEntityWith<SceneComponent, NewSceneMarkerComponent>(out var sceneComponent, out var newSceneMarkerComponent);
             sceneComponent.Scene = scene.GetController<SceneController>();
         }
         
@@ -70,12 +69,11 @@ namespace StubbUnity.Services
         public void LoadingComplete(in ISceneLoadingProgress progress)
         {
             var config = progress.Config;
-            var sceneName = config.Name.FullName;
             
             foreach (var idx in _newScenesFilter)
             {
                 var controller = _newScenesFilter.Get1[idx].Scene;
-                if (controller.SceneName.FullName == sceneName)
+                if (controller.SceneName.Equals(config.Name))
                 {
                     if (config.IsActive)
                     {
