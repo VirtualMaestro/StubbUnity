d8fd4162cd5755e264e88f37f4489d68007fed5e
VirtualMaestro <virtualmaestro@gmail.com>
2020-04-22 11:55:10 +0200

    Forgotten fix

--- StubbUnity/StubbUnity/Src/Extensions/WorldExtension.cs
+++ StubbUnity/StubbUnity/Src/Extensions/WorldExtension.cs
@@ -1,19 +1,18 @@
 using Leopotam.Ecs;
 using StubbFramework.Common.Names;
-using StubbFramework.Scenes;
 using UnityEngine.SceneManagement;
 
 namespace StubbUnity.Extensions
 {
     public static class WorldExtension
     {
-        public static bool HasScenes(this EcsWorld world, in IAssetName sceneName)
+        public static bool HasScene(this EcsWorld world, in IAssetName sceneName)
         {
             for (var i = 1; i < SceneManager.sceneCount; i++)
             {
                 var scene = SceneManager.GetSceneAt(i);
-                var controller = scene.GetController<ISceneController>();
-                
+                var controller = scene.GetController();
+
                 if (controller != null && controller.SceneName.Equals(sceneName))
                 {
                     return true;
