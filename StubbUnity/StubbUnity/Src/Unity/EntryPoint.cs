7426ba870161c2b3a1e07b3fdb14cc9ea4b20596
VirtualMaestro <virtualmaestro@gmail.com>
2021-01-25 20:54:19 +0100

    Fixed bug - move init of contexts in the Awake phase.

--- StubbUnity/StubbUnity/Src/Unity/EntryPoint.cs
+++ StubbUnity/StubbUnity/Src/Unity/EntryPoint.cs
@@ -2,6 +2,7 @@ using Leopotam.Ecs;
 using StubbUnity.StubbFramework;
 using StubbUnity.StubbFramework.Logging;
 using StubbUnity.StubbFramework.Physics;
+using StubbUnity.Unity.Contexts;
 using StubbUnity.Unity.Debugging;
 using StubbUnity.Unity.Logging;
 using UnityEngine;
@@ -10,23 +11,29 @@ namespace StubbUnity.Unity
 {
     public class EntryPoint : MonoBehaviour
     {
+        [SerializeField] 
+        private UnityContext context;
+        
+        [SerializeField] 
+        private UnityPhysicsContext physicsContext;
+        
         private IStubbContext _context;
         private IPhysicsContext _physicsContext;
 
-        private void Start()
+        private void Awake()
         {
             log.AddAppender(UnityLogAppender.LogDelegate);
-            _context = GetComponent<IStubbContext>();
+            _context = context == null ? GetComponent<IStubbContext>() : context;
             log.Assert(_context != null,
                 "Context is missing! Attach UnityContext to the GameObject where EntryPoint script is attached!");
             _context?.Init(new EcsWorld(), new UnityEcsDebug());
-
-            _physicsContext = GetComponent<IPhysicsContext>();
+            
+            _physicsContext = physicsContext == null ? GetComponent<IPhysicsContext>() : physicsContext;
             _physicsContext?.Init(_context.World);
             
             DontDestroyOnLoad(gameObject);
         }
-
+        
         private void Update()
         {
             _context.Run();
