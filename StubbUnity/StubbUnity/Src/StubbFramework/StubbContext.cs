f1d31f424c244b323c06e4577f791c52886722df
VirtualMaestro <virtualmaestro@gmail.com>
2021-02-27 18:02:23 +0100

    Forgotten context to prev commit

--- StubbUnity/StubbUnity/Src/StubbFramework/StubbContext.cs
+++ StubbUnity/StubbUnity/Src/StubbFramework/StubbContext.cs
@@ -5,21 +5,54 @@ namespace StubbUnity.StubbFramework
 {
     public class StubbContext : IStubbContext
     {
-        public bool IsDisposed { get; }
-        public void Dispose()
+        private EcsWorld _world;
+        private EcsSystems _rootSystems;
+        private IStubbDebug _debugger;
+
+        public EcsFeature HeadSystemFeature { get; set; }
+        public EcsFeature UsersFeature { get; set; }
+        public EcsFeature TailSystemFeature { get; set; }
+
+        public bool IsDisposed => _world == null;
+        public EcsWorld World => _world;
+
+        public StubbContext()
         {
-            throw new System.NotImplementedException();
+            Stubb.AddContext(this);
+
+            HeadSystemFeature = new SystemHeadFeature();
+            TailSystemFeature = new SystemTailFeature();
         }
 
-        public EcsWorld World { get; }
         public void Init(EcsWorld world, IStubbDebug debug = null)
         {
-            throw new System.NotImplementedException();
+            _world = world;
+            _debugger = debug;
+            _rootSystems = new EcsSystems(World, "RootSystems");
+
+            HeadSystemFeature?.Init(_world, _rootSystems, _rootSystems);
+            UsersFeature?.Init(_world, _rootSystems, _rootSystems);
+            TailSystemFeature?.Init(_world, _rootSystems, _rootSystems);
+
+            _debugger?.Init(_rootSystems, _world);
+
+            _rootSystems.ProcessInjects();
+            _rootSystems.Init();
         }
 
         public void Run()
         {
-            throw new System.NotImplementedException();
+            _rootSystems.Run();
+            _debugger?.Debug();
+        }
+
+        public void Dispose()
+        {
+            _rootSystems.Destroy();
+            _world.Destroy();
+
+            _world = null;
+            _rootSystems = null;
         }
     }
 }
